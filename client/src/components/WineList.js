d6ffddd7fafb5ae156c2996e051ad6c110321c5c
jbcastro <jbeckcastro810@southseattle.edu>
2019-08-16 13:33:51 -0700

    small fixes

--- client/src/components/WineList.js
+++ client/src/components/WineList.js
@@ -1,8 +1,10 @@
 import React from "react";
+import "./styles/App.css";
+
 const WineList = props => {
   const content = props.glasses.map(glass => (
     <div key={glass.winenum}>
-      <h3>wine name={glass.name}</h3>
+      {/* <h3>wine name={glass.name}</h3>
       <h3>wine grape1={glass.grape1}</h3>
       <h3>wine grape2={glass.grape2}</h3>
       <h3>wine grape3={glass.grape3}</h3>
@@ -18,10 +20,22 @@ const WineList = props => {
       <h3>wine description4={glass.description4}</h3>
       <h3>wine description5={glass.description5}</h3>
       <h3>wine description6={glass.description6}</h3>
-      <h3>wine funfact={glass.funfact}</h3>
+      <h3>wine funfact={glass.funfact}</h3> */}
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Grape 1</th>
+        </tr>
+        <tr>
+          <td>{glass.name}</td>
+        </tr>
+        <tr>
+          <td>{glass.grape1}</td>
+        </tr>
+      </table>
     </div>
   ));
 
-  return <div>{content}</div>;
+  return <ul>{content}</ul>;
 };
 export default WineList;
